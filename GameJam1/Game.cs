45508f912140ce1f0ca63cdb2c79d84b2af0b712
Peyton <peytonsemail2000@gmail.com>
2021-10-08 18:06:41 -0500

    Initial

---
+++ GameJam1/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam1
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Console-Jam
+GameJam1
