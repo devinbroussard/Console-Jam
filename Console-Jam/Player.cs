6dbb7b38378365111e04a79641181106bf57c33a
Nicholas <s218019@student.aie.edu>
2021-10-08 22:39:55 -0500

    Expanded the Player Class to Include an Inventory

--- Console-Jam/Player.cs
+++ Console-Jam/Player.cs
@@ -7,9 +7,14 @@ namespace Console_Jam
 {
     class Player : Entity
     {
-        public Player(char icon, Vector2 position, string name) : base (icon, position, name)
-        {
+        private Item[] _inventory;
+        private int _currentItem;
+        private Item[] _equippedItems;
 
+        public Player(float health, float attackPower, float defensePower, char icon, Vector2 position, string name) 
+            : base(health, attackPower, defensePower, icon, position, name)
+        {
+            
         }
     }
 }
