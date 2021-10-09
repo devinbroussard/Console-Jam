d006de7ab4ebffdf2ef9e0441c8a845155355843
Peyton <peytonsemail2000@gmail.com>
2021-10-09 13:48:56 -0500

    Created One More Enemy Animation

--- Console-Jam/Animation.cs
+++ Console-Jam/Animation.cs
@@ -308,5 +308,60 @@ namespace Console_Jam
 
             Console.Clear();
         }
+
+        public void Cookie()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 8; j > 6; j--)
+                {
+                    Console.SetCursorPosition(60, j - 2);
+                    Console.WriteLine("                                               ");
+                    Console.SetCursorPosition(60, j - 1);
+                    Console.Write("          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██          ");
+                    Console.SetCursorPosition(60, j + 0);
+                    Console.Write("        ██░░▓▓▓▓░░░░░░░░░░░░░░░░░░░░██        ");
+                    Console.SetCursorPosition(60, j + 1);
+                    Console.Write("       ██▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒         ");
+                    Console.SetCursorPosition(60, j + 2);
+                    Console.Write("      ▓▓░░░░░░░░░░░░░░░░░░░░░░░░▓▓░░░░░░░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 3);
+                    Console.Write("      ██▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▒▒     ");
+                    Console.SetCursorPosition(60, j + 4);
+                    Console.Write("      ██░░░░▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░░░▓▓▓▓▒▒        ");
+                    Console.SetCursorPosition(60, j + 5);
+                    Console.Write("      ██░░░░▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░░░▓▓▓▓▒▒        ");
+                    Console.SetCursorPosition(60, j + 6);
+                    Console.Write("      ██░░░░▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░░░░░░░▒▒       ");
+                    Console.SetCursorPosition(60, j + 7);
+                    Console.Write("      ██░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓░░░░░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 8);
+                    Console.Write("      ██░░▓▓░░░░░░░░░░░░░░░░░░░░▓▓▓▓░░▓▓░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 9);
+                    Console.Write("      ██░░██▓▓    ░░░░░░░░░░░░░░██    ██░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 10);
+                    Console.Write("      ██░░░░████  ░░░░░░░░░░░░░░████  ░░░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 11);
+                    Console.Write("      ██░░░░██████░░██░░░░░░██░░██████░░░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 12);
+                    Console.Write("      ██░░░░░░░░░░░░░░▓▓▓▓▓▓░░░░░░░░░░░░░░▒▒        ");
+                    Console.SetCursorPosition(60, j + 13);
+                    Console.Write("        ██▓▓▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓          ");
+                    Console.SetCursorPosition(60, j + 14);
+                    Console.Write("           ██▓▓░░░░░░░░░░░░░░░░░░▓▓░░░░██            ");
+                    Console.SetCursorPosition(60, j + 15);
+                    Console.Write("             ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓░░            ");
+                    Console.SetCursorPosition(60, j + 16);
+                    Console.Write("                                               ");
+                    Console.SetCursorPosition(0, 0);
+
+
+
+                    Thread.Sleep(300);
+                }
+            }
+
+            Console.Clear();
+        }
     }
 }
