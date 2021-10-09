ef7cc1906cb1f5974d52e2629e814220dcf82f83
Nicholas <s218019@student.aie.edu>
2021-10-08 19:29:59 -0500

    Created a Player Class and  a Basic Constructor for It

---
+++ Console-Jam/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Math_Library;
+
+namespace Console_Jam
+{
+    class Player : Actor
+    {
+        public Player(char icon, Vector2 position, string name) : base (icon, position, name)
+        {
+
+        }
+    }
+}
